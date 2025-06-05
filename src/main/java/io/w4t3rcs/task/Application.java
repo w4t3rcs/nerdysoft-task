a7622fb5394d737a3d307612ddfe2381c3ff929c
w4t3rcs <w4t3rofficial@gmail.com>
2025-06-05 11:54:30 +0200

    Initial commit

---
+++ src/main/java/io/w4t3rcs/task/Application.java
@@ -0,0 +1,13 @@
+package io.w4t3rcs.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdysoftTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NerdysoftTaskApplication.class, args);
+    }
+
+}
